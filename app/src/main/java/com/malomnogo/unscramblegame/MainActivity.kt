af8e7f783d559bb5bee40cced1a37c4561622282
Eugene <auricamercer@gmail.com>
2024-01-19 14:18:39 +0300

    MainActivity

--- app/src/main/java/com/malomnogo/unscramblegame/MainActivity.kt
+++ app/src/main/java/com/malomnogo/unscramblegame/MainActivity.kt
@@ -2,10 +2,32 @@ package com.malomnogo.unscramblegame
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doAfterTextChanged
+import com.malomnogo.unscramblegame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val viewModel = GameViewModel()
+
+        binding.submitButton.setOnClickListener {
+            val uiState = viewModel.submit(binding.inputEditText.text.toString())
+            uiState.show(binding)
+        }
+        binding.skipButton.setOnClickListener {
+            val uiState = viewModel.skip()
+            uiState.show(binding)
+        }
+        binding.inputEditText.doAfterTextChanged {
+            val uiState = viewModel.update(binding.inputEditText.text.toString())
+            uiState.show(binding)
+        }
+
+        val uiState = viewModel.init()
+        uiState.show(binding)
+
     }
 }
